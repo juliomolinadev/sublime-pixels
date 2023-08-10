8e52499db82822a66596bf22842070bdfb036f34
Julz Molina <jmolina.dex@gmail.com>
2023-08-09 20:54:12 -0700

    feat: :lipstick: Add batch component

--- src/site/components/Batch.tsx
+++ src/site/components/Batch.tsx
@@ -2,23 +2,36 @@ import { Item } from "./Item";
 
 export const Batch = () => {
 	interface Item {
-		imgURL: string;
+		// imgURL: string;
 		id: string;
-		title: string;
-		link: string;
+		// title: string;
+		// link: string;
 	}
 
+	// const items: Item[] = [
+	// 	{
+	// 		imgURL: "...",
+	// 		id: "123",
+	// 		title: "Image",
+	// 		link: "",
+	// 	},
+	// ];
+
 	const items: Item[] = [
-		{
-			imgURL: "...",
-			id: "123",
-			title: "Image",
-			link: "",
-		},
+		{ id: "01" },
+		{ id: "02" },
+		{ id: "03" },
+		{ id: "04" },
+		{ id: "05" },
+		{ id: "06" },
+		{ id: "07" },
+		{ id: "08" },
+		{ id: "09" },
+		{ id: "10" },
 	];
 
 	return (
-		<div>
+		<div className="batch">
 			{items.map((item) => (
 				<Item key={item.id}>{JSON.stringify(item)}</Item>
 			))}
