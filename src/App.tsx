dcaad0379ec5dc5477a9b46b4f691bcf74e7000e
Julz Molina <jmolina.dex@gmail.com>
2023-08-10 18:45:36 -0700

    feat: :lipstick: Add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Batch } from "./site/components/Batch";
+import { PaginationControls } from "./site/components/PaginationControls";
 
 export const App = () => {
 	return (
 		<div className="App">
 			<Batch />
+			<PaginationControls />
 		</div>
 	);
 };
---
+++ src/site/components/PaginationControls.tsx
@@ -0,0 +1,5 @@
+export const PaginationControls = () => {
+	const pagination = "<< 1, 2, 3, 4, 5 ... >>";
+
+	return <div>{pagination}</div>;
+};
