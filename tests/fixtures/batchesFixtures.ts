8a30f61787f7e9f85736c7770f2718e9709f7208
Julz Molina <jmolina.dex@gmail.com>
2023-09-19 16:02:09 -0700

    feat(State): :construction: Add batches slice

---
+++ tests/fixtures/batchesFixtures.ts
@@ -0,0 +1,79 @@
+export const batchesInitialState = {
+	"1": {
+		id: "1",
+		downloadables: 1,
+		name: "Batch 1",
+		items: {
+			"1": {
+				id: "1",
+				batch: "1",
+				img: "https://i.etsystatic.com/44663047/r/il/e6a1ea/5066928204/il_1140xN.5066928204_6l7x.jpg",
+				title: "Title 1",
+				buyLink: "https://www.etsy.com",
+				downloadFiles: ["https://www.firebase.com/bucket", "https://www.firebase.com/bucket"],
+				likes: 0,
+				disLikes: 0,
+				downloads: 0,
+			},
+		},
+	},
+};
+
+export const newBatch = {
+	id: "2",
+	downloadables: 1,
+	name: "Batch 2",
+	items: {
+		"2": {
+			id: "2",
+			batch: "2",
+			img: "https://i.etsystatic.com/44663047/r/il/e6a1ea/5066928204/il_1140xN.5066928204_6l7x.jpg",
+			title: "Title 2",
+			buyLink: "https://www.etsy.com",
+			downloadFiles: ["https://www.firebase.com/bucket", "https://www.firebase.com/bucket"],
+			likes: 1,
+			disLikes: 0,
+			downloads: 0,
+		},
+	},
+};
+
+export const batchesNewState = {
+	"1": {
+		id: "1",
+		downloadables: 1,
+		name: "Batch 1",
+		items: {
+			"1": {
+				id: "1",
+				batch: "1",
+				img: "https://i.etsystatic.com/44663047/r/il/e6a1ea/5066928204/il_1140xN.5066928204_6l7x.jpg",
+				title: "Title 1",
+				buyLink: "https://www.etsy.com",
+				downloadFiles: ["https://www.firebase.com/bucket", "https://www.firebase.com/bucket"],
+				likes: 0,
+				disLikes: 0,
+				downloads: 0,
+			},
+		},
+	},
+
+	"2": {
+		id: "2",
+		downloadables: 1,
+		name: "Batch 2",
+		items: {
+			"2": {
+				id: "2",
+				batch: "2",
+				img: "https://i.etsystatic.com/44663047/r/il/e6a1ea/5066928204/il_1140xN.5066928204_6l7x.jpg",
+				title: "Title 2",
+				buyLink: "https://www.etsy.com",
+				downloadFiles: ["https://www.firebase.com/bucket", "https://www.firebase.com/bucket"],
+				likes: 1,
+				disLikes: 0,
+				downloads: 0,
+			},
+		},
+	},
+};
--- tests/store/batches/batchesSlice.test.ts
+++ tests/store/batches/batchesSlice.test.ts
@@ -1,15 +1,21 @@
 import { describe, expect } from "vitest";
 import { AnyAction } from "@reduxjs/toolkit";
 import { it } from "vitest";
-import { batchInitialState } from "../../fixtures/batchFixtures";
-import { batchSlice } from "../../../src/store/batches/batchesSlice";
+import { batchesInitialState, batchesNewState, newBatch } from "../../fixtures";
+import { addBatch, batchSlice } from "../../../src/store/batches/batchesSlice";
 
 describe("batchSlice.ts tests", () => {
 	it("should return the initial state and be called batch", () => {
 		const action: AnyAction = { type: "" };
-		const state = batchSlice.reducer(batchInitialState, action);
+		const state = batchSlice.reducer(batchesInitialState, action);
 
 		expect(batchSlice.name).toBe("batch");
-		expect(state).toEqual(batchInitialState);
+		expect(state).toEqual(batchesInitialState);
+	});
+
+	it("should add a batch", () => {
+		const state = batchSlice.reducer(batchesInitialState, addBatch(newBatch));
+
+		expect(state).toEqual(batchesNewState);
 	});
 });
