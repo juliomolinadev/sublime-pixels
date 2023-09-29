3c68cdd347c8dfad8e9f53e1c81847f0060f5bd3
Julz Molina <jmolina.dex@gmail.com>
2023-09-29 14:56:53 -0700

    feat(CRUD): :construction: Add array update functionaliti in firestore

--- src/firebase/firestoreCRUD/updateDocOnFirestore.ts
+++ src/firebase/firestoreCRUD/updateDocOnFirestore.ts
@@ -1,15 +1,47 @@
-import { updateDoc, doc, WithFieldValue, DocumentData } from "firebase/firestore";
+import {
+	updateDoc,
+	doc,
+	WithFieldValue,
+	DocumentData,
+	arrayUnion,
+	arrayRemove,
+} from "firebase/firestore";
 import { FirebaseDB } from "../firebaseConfig";
+
+interface ArrayUpdate {
+	fieldName: string;
+	value: string | number | boolean;
+}
 
 interface Params {
 	collectionPath: string;
 	updates: WithFieldValue<DocumentData>;
 	docId: string;
+	arrayUnionUpdate?: ArrayUpdate;
+	arrayRemoveUpdate?: ArrayUpdate;
 }
 
-export const updateDocInFirestore = async ({ collectionPath, docId, updates }: Params) => {
+export const updateDocInFirestore = async ({
+	collectionPath,
+	docId,
+	updates,
+	arrayUnionUpdate,
+	arrayRemoveUpdate,
+}: Params) => {
 	try {
 		const docRef = doc(FirebaseDB, collectionPath, docId);
+
+		if (arrayUnionUpdate) {
+			await updateDoc(docRef, { [arrayUnionUpdate.fieldName]: arrayUnion(arrayUnionUpdate.value) });
+			return true;
+		}
+
+		if (arrayRemoveUpdate) {
+			await updateDoc(docRef, {
+				[arrayRemoveUpdate.fieldName]: arrayRemove(arrayRemoveUpdate.value),
+			});
+			return true;
+		}
 
 		await updateDoc(docRef, updates);
 
--- tests/firebase/firestoreCRUD/updateDocOnFirestore.test.ts
+++ tests/firebase/firestoreCRUD/updateDocOnFirestore.test.ts
@@ -7,7 +7,7 @@ import { readDocFromFirestore } from "../../../src/firebase/firestoreCRUD/readDo
 
 describe("updateDocOnFirestore() tests", () => {
 	it("should create and update a document on firestore", async () => {
-		const document = { id: "updatable", name: "test" };
+		const document = { id: "updatable", name: "test", testArray: ["testString1"] };
 		const createQuery = {
 			collectionPath: "tests/createDoc/createSubcollection",
 			docId: "updatable",
@@ -33,5 +33,45 @@ describe("updateDocOnFirestore() tests", () => {
 		expect(createResp).toBeTruthy();
 		expect(updateResp).toBeTruthy();
 		expect(name).toBe("test updated");
+	});
+
+	it("should update an array on a document on firestore with arrayUnion", async () => {
+		const updateQuery = {
+			collectionPath: "tests/createDoc/createSubcollection",
+			docId: "updatable",
+			updates: {},
+			arrayUnionUpdate: { fieldName: "testArray", value: "testString2" },
+		};
+		const updateResp = await updateDocInFirestore(updateQuery);
+
+		const readQuery = { collectionPath: "tests/createDoc/createSubcollection", docId: "updatable" };
+		const readResp = await readDocFromFirestore(readQuery);
+
+		let testArray: string[] = [];
+		if (readResp) testArray = readResp.data().testArray;
+
+		expect(updateResp).toBeTruthy();
+		expect(readResp).toBeTruthy();
+		expect(testArray).toContain("testString2");
+	});
+
+	it("should update an array on a document on firestore with arrayRemove", async () => {
+		const updateQuery = {
+			collectionPath: "tests/createDoc/createSubcollection",
+			docId: "updatable",
+			updates: {},
+			arrayRemoveUpdate: { fieldName: "testArray", value: "testString2" },
+		};
+		const updateResp = await updateDocInFirestore(updateQuery);
+
+		const readQuery = { collectionPath: "tests/createDoc/createSubcollection", docId: "updatable" };
+		const readResp = await readDocFromFirestore(readQuery);
+
+		let testArray: string[] = [];
+		if (readResp) testArray = readResp.data().testArray;
+
+		expect(updateResp).toBeTruthy();
+		expect(readResp).toBeTruthy();
+		expect(testArray).not.toContain("testString2");
 	});
 });
