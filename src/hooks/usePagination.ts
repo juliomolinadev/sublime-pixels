cfc43809ba62d6c41ecfb95544d7c9bb6f6c2735
Julz Molina <jmolina.dex@gmail.com>
2023-11-29 20:49:39 -0800

    fix(Ui): :bug: Fixed problem in pagination controls

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,18 +1,30 @@
 import { useEffect, useState } from "react";
 
 export const usePagination = (batches: string[]) => {
-	const [centralBatch, setCentralBatch] = useState(batches[2]);
-	const [visibleBatches, setVisibleBatches] = useState<string[]>(batches);
+	const [centralBatch, setCentralBatch] = useState<string | null>(
+		batches.length >= 5 ? batches[2] : null,
+	);
+	const [visibleBatches, setVisibleBatches] = useState<string[]>(
+		batches.length >= 6 ? batches.slice(0, 5) : batches,
+	);
 
 	useEffect(() => {
+		setCentralBatch(batches.length >= 5 ? batches[2] : null);
+		setVisibleBatches(batches.length >= 6 ? batches.slice(0, 5) : batches);
+	}, [batches]);
+
+	useEffect(() => {
+		if (!centralBatch) return;
+
 		const indexOfCentralBatch = batches.indexOf(centralBatch);
 		const batchesShown = batches.slice(indexOfCentralBatch - 2, indexOfCentralBatch + 3);
 
-		if (batches.length < 5) setVisibleBatches(batches);
-		else setVisibleBatches(batchesShown);
+		setVisibleBatches(batchesShown);
 	}, [centralBatch, batches]);
 
 	const onMoveOne = (positions: number): void => {
+		if (!centralBatch) return;
+
 		const index = batches.indexOf(centralBatch);
 		const newPosition = index + positions;
 
--- src/site/components/Header.tsx
+++ src/site/components/Header.tsx
@@ -5,10 +5,11 @@ import { PaginationControls } from "./PaginationControls";
 
 export const Header = () => {
 	const { uid } = useTypedSelector((state) => state.auth);
+	const { batchesArray } = useTypedSelector((state) => state.batches);
 
 	return (
 		<div className="header">
-			<PaginationControls />
+			{batchesArray.length > 0 && <PaginationControls />}
 
 			{uid ? <DownloadsCounter /> : <Invitation />}
 		</div>
